ff9676c8ca3bd90a48baa1de7803ea1d8015d0fb
MihailJP <mihailjp@gmail.com>
2023-09-21 19:53:07 +0900

    Apply patch

--- makeglyph/makeglyph.js
+++ makeglyph/makeglyph.js
@@ -2,6 +2,7 @@
 // shotai ... mincho or gothic
 // weight ... 1 3 5 7
 
+load("engine/2d.js");
 load("engine/polygon.js");
 load("engine/polygons.js");
 load("engine/buhin.js");
@@ -11,7 +12,15 @@ load("engine/kagedf.js");
 
 kage = new Kage();
 
-if(arguments[2] == "gothic"){
+if (arguments === undefined) {
+  if (scriptArgs !== undefined) { // workaround for newer mozjs
+    var arguments = scriptArgs;
+  }
+}
+
+if(arguments[2] == "socho"){
+  kage.kShotai = kage.kSocho;
+}else if(arguments[2] == "gothic"){
   kage.kShotai = kage.kGothic;
 } else {
   kage.kShotai = kage.kMincho;
@@ -19,21 +28,38 @@ if(arguments[2] == "gothic"){
 
 if(arguments[3] == 1){
   kage.kMinWidthY = 1;
+  kage.kMinWidthU = 1;
   kage.kMinWidthT = 4;
   kage.kWidth = 3;
   kage.kKakato = 4;
+} else if(arguments[3] == 4){
+  kage.kMinWidthY = 2.5;
+  kage.kMinWidthU = 2.5;
+  kage.kMinWidthT = 7;
+  kage.kWidth = 6;
+  kage.kKakato = 2.5;
+  kage.kMage = 11.5;
 } else if(arguments[3] == 5){
   kage.kMinWidthY = 3;
+  kage.kMinWidthU = 3;
   kage.kMinWidthT = 8;
   kage.kWidth = 7;
   kage.kKakato = 2;
+  kage.kMage = 13;
+  kage.kAdjustTateStep = 5;
+  kage.kAdjustMageStep = 6;
 } else if(arguments[3] == 7){
   kage.kMinWidthY = 4;
+  kage.kMinWidthU = 4;
   kage.kMinWidthT = 10;
   kage.kWidth = 9;
   kage.kKakato = 1;
+  kage.kMage = 17;
+  kage.kAdjustTateStep = 6;
+  kage.kAdjustMageStep = 7;
 } else {
   kage.kMinWidthY = 2;
+  kage.kMinWidthU = 2;
   kage.kMinWidthT = 6;
   kage.kWidth = 5;
   kage.kKakato = 3;
